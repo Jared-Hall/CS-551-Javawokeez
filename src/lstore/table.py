9d41c545880bc5753148846e544b0aca54cb527a
jodonnell221 <JohnOD221@proton.me>
2024-10-28 22:23:45 -0700

    Update tables.py
    Updated table.py to have insert()

--- src/lstore/table.py
+++ src/lstore/table.py
@@ -1,5 +1,6 @@
 from lstore.index import Index
 from time import time
+from lstore.page import Page
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -13,6 +14,7 @@ class Record:
         self.rid = rid
         self.key = key
         self.columns = columns
+        self.indirection = None
 
 
 class Table:
@@ -28,9 +30,37 @@ class Table:
         self.num_columns = num_columns
         self.page_directory = {}
         self.index = Index(self)
+        self.base_pages = [[Page()] for i in range(num_columns)]
+        self.tail_pages = [[Page()] for i in range(num_columns)]
+        self.bp_directory = {}
+        self.tp_directory = {}
+        self.bp_index = [0] * num_columns
+        self.tp_index = [0] * num_columns
+
         pass
+
+    def insert(self, *columns):
+        rid = []
+        for i, value in enumerate(columns):
+            if self.base_pages[i][self.bp_index[i]].has_capacity():
+                self.base_pages[i][self.bp_index[i]].write(value)
+                rid.append((self.bp_index[i],  self.base_pages[i][self.bp_index[i]].numEntries - 1)) 
+            else:
+                self.base_pages[i].append(Page()) 
+                self.bp_index[i] += 1 
+                self.base_pages[i][self.bp_index[i]].write(value)
+                rid.append((self.bp_index[i],  self.base_pages[i][self.bp_index[i]].numEntries - 1)) 
+        record = Record(rid, columns[0], columns)
+        self.bp_directory[rid] = record 
+        self.index.add(rid, columns[0])
+
+
+       
+    
+    
 
     def __merge(self):
         print("merge is happening")
         pass
+    
  
